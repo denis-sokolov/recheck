4fbd288f4ae6cb2df6ee2b395d05d2245378c5fd
MakeNowJust <MakeNowJust@users.noreply.github.com>
2021-02-09 13:10:27 +0000

    deploy: a8c69ebce360755557dae8a079e66a078095eafb

--- main.min.799dc1f94a7ec8545abd00b839d47ac7186e26fe8315706d23fd5a75ca837bfd.js
+++
@@ -1,7 +0,0 @@
-(()=>{var workerJS="https://makenowjust-labo.github.io/recheck/worker.min.deaad7a517f86ad6b2c78b31e0ee4af7d53455bd967793318447e2f2a2924fd4.js";var worker=(()=>{let webWorker=null;return{run(input){if(webWorker===null){webWorker=new Worker(workerJS);}
-webWorker.postMessage({input});return new Promise((resolve)=>{const handle=(e)=>{resolve(e.data);webWorker.removeEventListener("message",handle);};webWorker.addEventListener("message",handle);});},cancel(){if(webWorker===null)
-return;webWorker.terminate();webWorker=null;}};})();window.checker=()=>({state:"init",input:"/^(a|a)*$/",checkedInput:"",checkedTime:0,checkedResult:null,async check(){if(this.state==="checking")
-return;this.state="checking";this.checkedInput="";this.checkedTime=0;this.checkedResult=null;const input=this.input;const startTime=Date.now();const result=await worker.run(input);const checkedTime=Date.now()-startTime;this.state="checked";this.checkedInput=input;this.checkedTime=checkedTime;this.checkedResult=result;},cancel(){this.state="init";this.checkedInput="";this.checkedTime=0;this.checkedResult=null;worker.cancel();},get checkedHotspot(){const input=this.checkedResult.source;let index=0;const parts=[];for(const{start,end,temperature}of this.checkedResult.hotspot){if(index<start)
-parts.push({text:input.substring(index,start)});parts.push({text:input.substring(start,end),temperature});index=end;}
-if(index<input.length)
-parts.push({text:input.substring(index)});return parts;}});})();
